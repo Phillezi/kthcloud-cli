48c80d45f2fbdf071e41f810702e12773f823c48
Philip Zingmark <philip.zingmark@gmail.com>
2024-09-10 15:58:32 +0200

    Track jobs

--- internal/compose/up.go
+++ internal/compose/up.go
@@ -1,8 +1,12 @@
 package compose
 
 import (
+	"errors"
+	"go-deploy/dto/v2/body"
 	"kthcloud-cli/internal/model"
 	"kthcloud-cli/pkg/util"
+	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -29,17 +33,63 @@ func Up(filename string) error {
 		log.Fatalln(err)
 	}
 
+	var wg sync.WaitGroup
+
 	for key, service := range services {
 		resp, err := session.Client.Req("/v2/deployments", "POST", serviceToDepl(service, key, projectDir))
 		if err != nil {
 			log.Errorln("error: ", err, " response: ", resp)
 			return err
 		}
-		if err := util.HandleResponse(resp); err != nil {
+		if resp.IsError() {
+			return errors.New("could not create deployment: " + key)
+		}
+		job, err := util.ProcessResponse[body.DeploymentCreated](resp.String())
+		if err != nil {
 			return err
 		}
-		log.Info("response: ", resp)
-		log.Info("Created deployment: ", key)
+
+		wg.Add(1)
+		go func(jobId string, serviceKey string) {
+			defer wg.Done()
+			log.Infof("Tracking job for deployment: %s", serviceKey)
+
+			for {
+				jobResp, err := session.Client.Req("/v2/jobs/"+jobId, "GET", nil)
+				if err != nil {
+					log.Errorf("Failed to get job status for deployment %s: %v", serviceKey, err)
+					return
+				}
+
+				jobStatus, err := util.ProcessResponse[body.JobRead](jobResp.String())
+				if err != nil {
+					log.Errorf("Error processing job status for deployment %s: %v", serviceKey, err)
+					return
+				}
+
+				//log.Infof("Job status for deployment %s: %v", serviceKey, jobStatus)
+
+				// Break out of loop when job is complete
+				if jobStatus.Status == "finished" {
+					log.Infof("Deployment %s created successfully", serviceKey)
+					break
+				}
+				if jobStatus.Status == "terminated" {
+					log.Infof("Job for deployment %s was terminated", serviceKey)
+					break
+				}
+				if jobStatus.LastError != nil {
+					log.Errorf("Failed to create deployment: %s", serviceKey)
+					break
+				}
+
+				time.Sleep(500 * time.Millisecond)
+			}
+		}(job.JobID, key)
+
 	}
+	wg.Wait()
+
+	log.Info("All jobs have been tracked and completed.")
 	return nil
 }
