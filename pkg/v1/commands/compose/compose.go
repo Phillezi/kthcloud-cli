c8598a19f842a61a99def7ad988b4ca3cbf68cbc
Philip Zingmark <philip.zingmark@gmail.com>
2024-09-19 23:17:42 +0200

    added compose down command

--- pkg/v1/commands/compose/compose.go
+++ pkg/v1/commands/compose/compose.go
@@ -95,7 +95,60 @@ func Parse() {
 }
 
 func Down() {
-	logrus.Fatal("not implemented yet...")
+	composeInstance, err := parser.GetCompose()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	c := client.Get()
+	if !c.HasValidSession() {
+		logrus.Fatal("login")
+	}
+
+	c.DropDeploymentsCache()
+	depls, err := c.Deployments()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	deploymentMap := make(map[string]*body.DeploymentRead)
+	for _, depl := range depls {
+		deploymentMap[depl.Name] = &depl
+	}
+
+	for name := range composeInstance.Services {
+		if depl, exists := deploymentMap[name]; exists {
+			c.Remove(depl)
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Color("blue")
+	s.Start()
+	defer s.Stop()
+
+	for name := range composeInstance.Services {
+		if deployment, exists := deploymentMap[name]; exists {
+			resp, err := c.Remove(deployment)
+			if err != nil {
+				logrus.Fatal(err)
+			}
+			err = response.IsError(resp.String())
+			if err != nil {
+				logrus.Fatal(err)
+			}
+			job, err := util.ProcessResponse[body.DeploymentDeleted](resp.String())
+			if err != nil {
+				logrus.Errorln(resp.String())
+				logrus.Fatal(err)
+			}
+			jobs.TrackDeploymentDeletionW(deployment.Name, job, &wg, s)
+		}
+	}
+	wg.Wait()
+	s.Color("green")
+	s.Stop()
 }
 
 func Logs() {
