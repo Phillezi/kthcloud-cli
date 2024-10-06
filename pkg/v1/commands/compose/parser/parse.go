252acf3a941f35a7663e5fe4d73e140ef1db39d9
Philip Zingmark <philip.zingmark@gmail.com>
2024-10-06 13:02:56 +0200

    specify compose file

--- pkg/v1/commands/compose/parser/parse.go
+++ pkg/v1/commands/compose/parser/parse.go
@@ -6,10 +6,15 @@ import (
 	"os"
 
 	"github.com/Phillezi/kthcloud-cli/pkg/v1/models/compose"
+	"github.com/spf13/viper"
 )
 
 func GetCompose() (*compose.Compose, error) {
-	files := []string{"docker-compose.yml", "docker-compose.yaml"}
+	files := []string{"kthcloud.docker-compose.yml", "kthcloud.docker-compose.yaml", "docker-compose.yml", "docker-compose.yaml"}
+	specifiedFile := viper.GetString("file")
+	if specifiedFile != "" {
+		files = []string{specifiedFile}
+	}
 
 	var filePath string
 	var fileFound bool
@@ -23,7 +28,12 @@ func GetCompose() (*compose.Compose, error) {
 	}
 
 	if !fileFound {
-		log.Println("No docker-compose file found.")
+		if specifiedFile != "" {
+			log.Println("No file " + specifiedFile + " found in current directory.")
+		} else {
+			log.Println("No docker-compose file found.")
+
+		}
 		return nil, errors.New("no docker-compose file found in current directory")
 	}
 
